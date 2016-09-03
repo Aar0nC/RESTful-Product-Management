12e53eef7db4a5b4bf5f3e5b0ac441a565a68571
Aaron Cotter <acotter@tfs.ca>
2016-09-03 15:45:55 -0400

    added schema.sql for production deployment

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS 'Products'(
+  `Name` varchar(100) NOT NULL,
+  `Description` varchar(500) DEFAULT NULL,
+  `Price` decimal(10,2) unsigned zerofill NOT NULL,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_UNIQUE` (`Id`)
+)
