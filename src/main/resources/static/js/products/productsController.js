959767746c26d00ab999e1f44746bad5e2652f39
Aaron Cotter <acotter@tfs.ca>
2016-09-24 13:02:11 -0400

    .gitignore updated

--- src/main/resources/static/js/products/productsController.js
+++ src/main/resources/static/js/products/productsController.js
@@ -1,9 +1,9 @@
 angular.module('products').controller('productsController', ['$scope', 'productRestFactory', 'dataFactory', function($scope, productRestFactory, dataFactory){
     $scope.product = {};
-    $scope.error = {};
     $scope.addProductBool = false;
     $scope.editProductBool = false;
     $scope.serverError = false;
+    $scope.serverSuccess = false;
 
     if(dataFactory.getProductCollection().length !== 0){
         //do not make another rest call
@@ -32,6 +32,9 @@ angular.module('products').controller('productsController', ['$scope', 'productR
         $scope.editProductBool = false;
         $scope.product = {};
         $scope.newProduct.$setPristine(true);
+        $scope.message = {};
+        $scope.serverError = false;
+        $scope.serverSuccess = false;
     };
 
     $scope.editProduct = function(product){
@@ -39,9 +42,6 @@ angular.module('products').controller('productsController', ['$scope', 'productR
         $scope.product = product;
     };
 
-    $scope.clearServerError = function(){
-        $scope.serverError = false;
-    };
 
     $scope.submitNewProduct = function(){
         productRestFactory.save($scope.product, function(response){
@@ -49,10 +49,12 @@ angular.module('products').controller('productsController', ['$scope', 'productR
             console.log(response);
             $scope.productCollection.push(response);
             dataFactory.setProductCollection(angular.copy($scope.productCollection));
+            $scope.serverSuccess = true;
+            $scope.message = "Product " + response.id + " inserted";
         }, function(error){
             console.log(error);
             $scope.serverError = true;
-            $scope.error = error;
+            $scope.message = error;
         });
         $scope.cancelAddProduct();
     };
@@ -65,6 +67,8 @@ angular.module('products').controller('productsController', ['$scope', 'productR
             var index = $scope.productCollection.indexOf(product);
             $scope.productCollection.splice(index, 1);
             dataFactory.setProductCollection(angular.copy($scope.productCollection));
+            $scope.serverSuccess = true;
+            $scope.message = "Product " + product.id + " deleted";
         }, function(error){
             console.log(error);
             $scope.serverError = true;
@@ -77,6 +81,8 @@ angular.module('products').controller('productsController', ['$scope', 'productR
         productRestFactory.update({id: $scope.product.id}, $scope.product, function(response){
             console.log("product updated");
             console.log(response);
+            $scope.serverSuccess = true;
+            $scope.message = "Product " + $scope.product.id + " edited";
         },function(error){
             console.log(error);
             $scope.serverError = true;
